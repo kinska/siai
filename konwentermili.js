27eb8c0018e86e05a490a525f8d3bea616b46172
kinska <kingaromanowska8@gmail.com>
2024-12-05 15:44:42 +0100

    konwentermili

---
+++ konwentermili.js
@@ -0,0 +1,3 @@
+let OdlegloscWMilach = 130;
+let OdlegloscWKilometrach = OdlegloscWMilach * 1.60934;
+console.log("Odległość " + OdlegloscWMilach + "mil odpowiada " + OdlegloscWKilometrach.toFixed(4) + " kilometra");
